07fb4b1a73a3429c8fa8107c630959d8f79f1a3a
Tatyana Chernyadieva <tany.taray@gmail.com>
2023-02-14 21:09:25 +0400

    Spring Framework (IoC)
    добавлена ветка test-di, где показана настройка bean через Spring в xml

--- src/main/java/ru/home/chernyadieva/spring/TestSpring.java
+++ src/main/java/ru/home/chernyadieva/spring/TestSpring.java
@@ -1,7 +1,6 @@
 package ru.home.chernyadieva.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.home.chernyadieva.music.interface_music.Music;
 import ru.home.chernyadieva.music.player.MusicPlayer;
 
 public class TestSpring {
@@ -13,13 +12,14 @@ public class TestSpring {
          */
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicClassicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);// IoC (внедрение зависимостей через конструктор)
-        System.out.println(musicPlayer.playMusic());
+        //Music music = context.getBean("musicClassicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);// IoC (внедрение зависимостей через конструктор)
 
-        Music music2 = context.getBean("musicRockBean", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);// IoC (внедрение зависимостей через конструктор)
-        System.out.println(musicPlayer2.playMusic());
+        /*
+        теперь делаем DI с помощью Spring через конфигурационный файл, ранее создавали bean вручную(см.комментарии выше)
+         */
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.playMusic());
 
         context.close();//закрываем контекст
     }
