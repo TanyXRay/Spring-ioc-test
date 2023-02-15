3370579f19b3e030bd70031a3cf534731809c24b
Tatyana Chernyadieva <tany.taray@gmail.com>
2023-02-15 23:51:57 +0400

    Spring Framework (IoC)
    Добавлена ветка test-di-fabric-method, где показано внедрение зависимости через фабричный метод

--- src/main/java/ru/home/chernyadieva/music/player/MusicPlayer.java
+++ src/main/java/ru/home/chernyadieva/music/player/MusicPlayer.java
@@ -1,16 +1,33 @@
 package ru.home.chernyadieva.music.player;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import ru.home.chernyadieva.music.interface_music.Music;
 
 public class MusicPlayer {
     private Music music;
 
-    //IoC
-    public MusicPlayer(Music music) {
+    public void setMusic(Music music) {
         this.music = music;
     }
 
-    public String playMusic(){
-       return "Playing: " + music.getSong();
+    private MusicPlayer() {
+    }
+
+    private void initBeforeBeanCreation() {
+        System.out.println("method of initialization before bean creation");
+    }
+
+    private void destroyBeforeClosingApp() {
+        System.out.println("method of destroy before closing the application");
+    }
+
+    @Contract(value = " -> new", pure = true)
+    public static @NotNull MusicPlayer getMusicPlayer() {
+        return new MusicPlayer();
+    }
+
+    public String playMusic() {
+        return "Playing: " + music.getSong();
     }
 }
